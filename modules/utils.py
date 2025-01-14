a0871c4ffbbb305a7b454118a393ab10d7c16112
MARCO <194879232+MARCO-UNIVERSE@users.noreply.github.com>
2025-01-14 12:12:36 +0530

    utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -92,7 +92,6 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▰" * completed_length + "▱" * remaining_length
             
             try:
-                await reply.edit(f'\n **╭──⌯════देखलो भाई 𝖀𝖕𝖑𝖔𝖆𝖉𝖎𝖓𝖌 चल रही है ═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n├𝕭𝖔𝖙 𝓜α∂𝒆 𝐁𝐲 » ＭＡＲＣＯ™ ☢♔\n╰─═══ ✪ Powered By MARCO_MAIN ✪ ═══─╯**\n') 
+                await reply.edit(f'\n**─《 🚀 ᴘʀᴏɢʀᴇꜱꜱ ʙᴀʀ 》─⪢ \n\n─────────────────\n● Progress   : {perc}\n● Done     : {cur}\n● Total    : {tot}\n● Speed    : {sp}\n● ETA      : {eta}\n● 𝕭𝖔𝖙 𝓜α∂𝒆 𝐁𝐲 » ＭＡＲＣＯ™ ☢♔\n─────────────────\n╰─ ✪ Powered By MARCO_MAIN ✪ ─╯**\n')
             except FloodWait as e:
                 time.sleep(e.x)
-
