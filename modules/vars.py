1d00fa992dfe9a89957627c71dfbbb46d07cb3da
mjxmeena <mrmanoj950@gmail.com>
2025-04-07 08:04:40 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = "23069582"
 api_hash = "b3b56eaf67828684f54d540f684fdf1f"
-bot_token = "7672004632:AAHohyBfssnv2Nlscz0tqiji3r1I7pfsdUc"
-AUTH = "7672004632"
+bot_token = "7734111923:AAEtbojLOPp5seA14V54NTAW-USvSoxGs0c"
+AUTH = "1780523256"
 #Bot Username = "nitish"
